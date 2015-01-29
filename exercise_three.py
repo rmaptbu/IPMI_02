ec6a8de4c6692b595d751d31799ad89aa44fc6d3
rmaptbu <rmaptbu@ucl.ac.uk>
2015-01-29 16:16:29 +0000

    solved excercise 3

--- exercise_three.py
+++ exercise_three.py
@@ -7,14 +7,25 @@
 import argparse
 import SimpleITK as sitk
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--img", required=True, help="Input image")
-parser.add_argument("-o", "--out", required=True, help="Result segmentation")
+#parser = argparse.ArgumentParser()
+#parser.add_argument("-i", "--img", required=True, help="Input image")
+#parser.add_argument("-o", "--out", required=True, help="Result segmentation")
+#
+#
+#args = parser.parse_args()
+#infile=args.img
+#outfile=args.out
 
+input=sitk.ReadImage("OtsuB2.png")
+filter=sitk.BinaryErodeImageFilter()
+filter.SetKernelRadius(5).SetForegroundValue(255)
+output=filter.Execute(input)
+sitk.WriteImage(output,"ErodeB2.png")
 
-args = parser.parse_args()
-infile=args.img
-outfile=args.out
+input=sitk.ReadImage("ErodeB2.png")
+filter=sitk.BinaryDilateImageFilter()
+filter.SetKernelRadius(5).SetForegroundValue(255)
+output=filter.Execute(input)
+sitk.WriteImage(output,"DilateErodeB2.png")
 
-#Your code goes here
 
